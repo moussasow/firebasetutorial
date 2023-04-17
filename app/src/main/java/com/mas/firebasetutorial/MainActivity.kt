108e8bb74986b3d799052e0c97859aa099895c89
moussa sow <sow.study@gmail.com>
2023-04-17 13:58:42 +0900

    Added realtime database

--- app/src/main/java/com/mas/firebasetutorial/MainActivity.kt
+++ app/src/main/java/com/mas/firebasetutorial/MainActivity.kt
@@ -2,7 +2,10 @@ package com.mas.firebasetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.mas.firebasetutorial.ui.fragment.BaseFragment
 import com.mas.firebasetutorial.ui.fragment.main.MainFragment
+import com.mas.firebasetutorial.ui.fragment.top.TopFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
+            val user = FirebaseAuth.getInstance().currentUser
+            val fragment: BaseFragment = if (user != null) {
+                TopFragment.newInstance(user.email, user.displayName)
+            } else {
+                MainFragment.newInstance()
+            }
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, fragment)
                 .commitNow()
         }
     }
