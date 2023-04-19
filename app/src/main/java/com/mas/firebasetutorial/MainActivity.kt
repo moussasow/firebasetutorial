9e41d3268da459762e6a3a05d175caac382f419a
moussa sow <sow.study@gmail.com>
2023-04-19 13:44:09 +0900

    add bottom navigation listener

--- app/src/main/java/com/mas/firebasetutorial/MainActivity.kt
+++ app/src/main/java/com/mas/firebasetutorial/MainActivity.kt
@@ -3,15 +3,21 @@ package com.mas.firebasetutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.mas.firebasetutorial.databinding.ActivityMainBinding
 import com.mas.firebasetutorial.ui.fragment.BaseFragment
+import com.mas.firebasetutorial.ui.fragment.login.LoginFragment
 import com.mas.firebasetutorial.ui.fragment.main.MainFragment
 import com.mas.firebasetutorial.ui.fragment.top.TopFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             val user = FirebaseAuth.getInstance().currentUser
             val fragment: BaseFragment = if (user != null) {
@@ -19,9 +25,38 @@ class MainActivity : AppCompatActivity() {
             } else {
                 MainFragment.newInstance()
             }
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, fragment)
-                .commitNow()
+            navigateFragment(fragment)
         }
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            val fragment: BaseFragment = when (item.itemId) {
+                R.id.nav_home -> {
+                    val user = FirebaseAuth.getInstance().currentUser
+                    if (user != null) {
+                        TopFragment.newInstance(user.email, user.displayName)
+                    } else {
+                        MainFragment.newInstance()
+                    }
+                }
+                R.id.nav_upload -> TopFragment()
+                R.id.nav_map -> LoginFragment()
+                R.id.nav_notification -> TopFragment()
+                R.id.nav_info -> LoginFragment()
+                else -> MainFragment.newInstance()
+            }
+
+            navigateFragment(fragment)
+            true
+        }
+    }
+
+    private fun navigateFragment(fragment: BaseFragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
     }
 }
